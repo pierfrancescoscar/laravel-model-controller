d7fe59749b2e6495c6b0af2e72ecaeab25ce0b89
pierfrancescoscar <scarinci401@gmail.com>
2022-01-21 15:12:15 +0100

    ADD: some spacing to card, centered all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
     <div class="container p-5">
         <div class="row">
             <div class="col-12">
-                <h3 class="mb-4">Movies Database</h3>
+                <h3 class="mb-5 text-center">Movies Database</h3>
             </div>
-        <div class="row">
+        <div class="row justify-content-center">
             @foreach ($movies as $movie)
-            <div class="col-2 card p-3">
+            <div class="col-2 me-3 mb-3 card p-3">
                 <h6>{{ $movie->title }}</h6>
                 <span class="d-block">{{ $movie->original_title }}</span>
                 <span class="d-block">{{ $movie->nationality }}</span>
