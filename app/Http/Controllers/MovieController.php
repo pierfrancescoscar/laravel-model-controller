5d667b3e54d03d1de3fe218a5a9a4cf71e12a9e7
pierfrancescoscar <scarinci401@gmail.com>
2022-01-21 14:47:16 +0100

    ADD: controller, model and layout partials

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class MovieController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+        // dump($movies);
+
+        return view('home');
+
+    }
+}
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue fixed-bottom">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 bg-dark">
+        <span class="text-white">© 2022 Copyright:</span>
+      <span class="text-white"> Made with love by Pierfrancesco S.</span>
+    </div>
+
+</footer>
+ 
